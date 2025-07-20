0d501d71c6389c2c3be6396644519426750fd3b1
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-07-20 01:54:59 -0700

    playing audio!

--- src/components/Radio.svelte
+++ src/components/Radio.svelte
@@ -1,5 +1,7 @@
 <script>
     let frequency = 101.2;
+    let power = false;
+
     const SEED = "radIO";
 
     // Static data to build the radio from
@@ -16,8 +18,7 @@
         },
     ];
 
-    const SEARCH_URL = (collection) =>
-        `https://archive.org/advancedsearch.php?q=collection:${collection}+AND+mediatype:audio+AND+format:MP3&rows=5&output=json`;
+    const SEARCH_URL = (collection) => `https://archive.org/advancedsearch.php?q=collection:${collection}+AND+mediatype:audio+AND+format:MP3&rows=5&output=json`;
 
     // Dynamic data for radio
     let radio = [];
@@ -27,7 +28,8 @@
     async function getStreamUrl(identifier) {
         const metadata = await fetch("https://archive.org/metadata/" + identifier).then(res => res.json())
         const streamUrl = `https://archive.org/download/${identifier}/${encodeURIComponent(metadata.files.find(file => file.format.includes("MP3")).name)}`
-        console.log(streamUrl)
+        // console.log(streamUrl)
+        return streamUrl
     }
 
     // Load radio data on component mount
@@ -51,6 +53,7 @@
                     frequency: station.frequency,
                     identifier: station.identifier,
                     items: items,
+                    streamUrl: streamUrl
                 };
             }),
         ).catch((err) => {
@@ -70,20 +73,26 @@
     // $: console.log(station);
 </script>
 
-{#if loading}
-    <div class="flex flex-col gap-4 p-4 w-full">
-        <p class="text-xl">Loading {STATIONS.length} radio stations...</p>
-    </div>
-{:else if error}
+
+{#if error}
     <div class="flex flex-col gap-4 p-4 w-full">
         <p class="text-xl">Error loading radio stations: {error}</p>
     </div>
 {:else}
     <div class="flex flex-col gap-4 p-4 w-full">
-        <label class="text-xl" for="frequency-input">
-            {frequency.toFixed(1)}
-            <span class="text-sm text-gray-500">{station?.name}</span>
-        </label>
+        {#if loading}
+            <div class="flex flex-col gap-4 p-4 w-full">
+                <p class="text-sm text-gray-500">Loading {STATIONS.length} radio stations...</p>
+            </div>
+        {/if}
+        <div>
+            <label class="text-xl" for="frequency-input">
+                {frequency.toFixed(1)}
+                <span class="text-sm text-gray-500">{station?.name}</span>
+            </label>
+            <input type="button" on:click={() => power = !power} value={power ? "🔊" : "🔇"} class=" ml-3 bg-gray-200 p-1 text-sm">
+            
+        </div>
         <input
             type="range"
             min="77"
@@ -93,6 +102,15 @@
             class="h-2 w-full md:w-1/2 lg:w-1/4 appearance-none bg-gray-200 cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-1 [&::-webkit-slider-thumb]:bg-orange-500 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-2 [&::-moz-range-thumb]:bg-orange-500 [&::-moz-range-thumb]:border-none"
             id="frequency-input"
         />
+
+        {#each radio as station}
+        <div>{station.identifier}</div>
+            <audio autoplay muted={!power} class="w-full md:w-1/2 lg:w-1/4">
+                <source src={station.streamUrl} type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        {/each}
+
         <!-- <iframe src="https://archive.org/embed/radiobooks&amp;autoplay=1" width="500" height="300" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe> -->
     </div>
 {/if}
